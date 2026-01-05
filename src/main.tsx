151f46e6b0e8ee332c8c7e28dc488ec348a69dca
Tiz Code <opawaletimi@gmail.com>
2026-01-05 19:36:09 +0100

    Fix import path for global CSS file

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { PlayerProvider } from "./context/PlayerContext.tsx";
-import "./styles/global.css";
+import "./Styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
